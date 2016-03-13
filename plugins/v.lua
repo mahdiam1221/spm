f8d968c589fd417b5b24b1aceac69c1d4febcba4
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-03-13 16:33:58 +0330

    Create v.lua

---
+++ plugins/v.lua
@@ -0,0 +1,14 @@
+function run(msg, matches)
+local text = "emc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\nemc spammer\n\n\n"
+return text
+end
+return {
+  description = "echo",
+  usage = {
+    "text",
+  },
+  patterns = {
+    "^(.*)$",
+  },
+  run = run,
+}
