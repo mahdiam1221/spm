06497072a0470b850475b708d773b506db7c3713
EMCTeamIR <EMCTeamIR@users.noreply.github.com>
2016-03-13 16:33:08 +0330

    Create b.lua

---
+++ plugins/b.lua
@@ -0,0 +1,14 @@
+function run(msg, matches)
+local text = matches[1]
+return text
+end
+return {
+  description = "echo",
+  usage = {
+    "text",
+  },
+  patterns = {
+    "^(.*)$",
+  },
+  run = run,
+}
